1b229273e231d85af41c85cd53320b7e039f9246
Ammar Zubair <ammarzubair@gmail.com>
2018-07-03 14:38:20 +0500

    favicon fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="icon" href="./favicon.ico" type="image/ico">
+        <link rel="icon" href="{{ url('/favicon.ico') }}" type="image/ico">
 
         <title>MyGerrys HRMS</title>
 
