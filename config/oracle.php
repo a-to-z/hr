73f11326ab77bbcb36edfa0df7b0917147a4cdc7
Ammar <ammarzubair@gmail.com>
2018-11-19 15:03:06 +0500

    new db

--- config/oracle.php
+++ config/oracle.php
@@ -4,9 +4,9 @@ return [
     'oracle' => [
         'driver'        => 'oracle',
         'tns'           => env('DB_TNS', ''),
-        'host'          => env('192.168.0.15', '192.168.0.15'),
+        'host'          => env('192.168.0.181', '192.168.0.181'),
         'port'          => env('DB_POR', '1521'),
-        'database'      => env('v10gn', 'v10gn'),
+        'database'      => env('r11gr2', 'r11gr2'),
         'username'      => env('mgphp', 'mgphp'),
         'password'      => env('mgry1319', 'mgry1319'),
         'charset'       => env('DB_CHARSET', 'AL32UTF8'),
